1574748bdb6547e669dc7c150ac341e3d703e9be
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2024-02-28 18:01:08 -0500

    modified backend.tf

--- backend.tf
+++ backend.tf
@@ -6,6 +6,5 @@ terraform {
     region = "us-east-1"
     dynamodb_table = "dynamodb-ducks-develop"
     encrypt = true  
-    ECR_REGISTRY_URI = "658299583140.dkr.ecr.us-east-1.amazonaws.com/duck-shop-0.1"
    }
 }
