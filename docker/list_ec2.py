07f958d34f69b3d9d399615740cc352a4d24936f
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2024-03-07 23:23:26 -0500

    docker/list_ec2.py

--- docker/list_ec2.py
+++ docker/list_ec2.py
@@ -7,23 +7,23 @@ load_dotenv()
 
 def obtener_detalles_todas_instancias():
     try:
-# Crea el cliente de EC2 usando las credenciales de las variables de entorno
-# Utiliza las del usuario que cree en IAM
+        # Crea el cliente de EC2 usando las credenciales de las variables de entorno
+        # Utiliza las del usuario que creó en IAM
         ec2_client = boto3.client(
             'ec2',
             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
         )
 
-# Describe todas las instancias sin filtrar por VPC
+        # Describe todas las instancias
         response = ec2_client.describe_instances()
 
-# Obtiene detalles específicos de las instancias
+        # Obtiene detalles específicos de las instancias
         for reservation in response['Reservations']:
             for instance in reservation['Instances']:
                 instance_id = instance['InstanceId']
 
-# Obtiene el nombre de la instancia si está disponible
+                # Obtiene el nombre de la instancia si está disponible
                 instance_name = ""
                 for tag in instance.get('Tags', []):
                     if tag['Key'] == 'Name':
@@ -36,5 +36,5 @@ def obtener_detalles_todas_instancias():
         print(f"Error al obtener instancias: {e}")
 
 if __name__ == "__main__":
-# Llama a la función para obtener detalles de todas las instancias
+    # Llama a la función para obtener detalles de todas las instancias
     obtener_detalles_todas_instancias()
