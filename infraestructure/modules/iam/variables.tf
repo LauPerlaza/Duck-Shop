c2e52edd6a7159877b1df8048e9abbfe03300674
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2024-03-08 04:11:32 -0500

    mejoras a infraestructure

--- infraestructure/modules/iam/variables.tf
+++ infraestructure/modules/iam/variables.tf
@@ -2,15 +2,3 @@ variable "name" {
   description = "Name for the IAM Role"
   type        = string
 }
-
-variable "attach_to" {
-  description = "the arn or role name to attach the policy created"
-  type        = string
-  default     = ""
-}
-
-variable "policy" {
-  description = "a json with the policy"
-  type        = string
-  default     = ""
-}
