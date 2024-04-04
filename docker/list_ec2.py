d0e4bf9ff169baf54e3951ea27d30fcb8695a562
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2024-04-04 13:04:23 -0500

    le adicione al script de python la lista de vpc

--- docker/list_ec2.py
+++ docker/list_ec2.py
@@ -5,36 +5,49 @@ import os
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
 
-def obtener_detalles_todas_instancias():
-    try:
-        # Crea el cliente de EC2 usando las credenciales de las variables de entorno
-        # Utiliza las del usuario que creó en IAM
-        ec2_client = boto3.client(
-            'ec2',
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-        )
+# Crea el cliente de EC2 usando las credenciales de las variables de entorno
+# Utiliza las del usuario que creó en IAM
+ec2_client = boto3.client(
+    'ec2',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
 
-        # Describe todas las instancias
-        response = ec2_client.describe_instances()
+# Obtiene la lista de todas las VPCs
+vpcs_response = ec2_client.describe_vpcs()
+vpcs = vpcs_response['Vpcs']
 
-        # Obtiene detalles específicos de las instancias
-        for reservation in response['Reservations']:
-            for instance in reservation['Instances']:
-                instance_id = instance['InstanceId']
+# Imprime información sobre todas las VPCs disponibles
+print("Lista de VPCs disponibles:")
+for vpc in vpcs:
+    print("VPC ID:", vpc['VpcId'])
+    print("CIDR Block:", vpc['CidrBlock'])
+    print("------------------------------------------")
 
-                # Obtiene el nombre de la instancia si está disponible
-                instance_name = ""
-                for tag in instance.get('Tags', []):
-                    if tag['Key'] == 'Name':
-                        instance_name = tag['Value']
-                        break
+# Solicita al usuario que ingrese la ID de la VPC que desea listar instancias
+vpc_id = input("Ingrese la ID de la VPC que desea listar instancias: ")
 
-                print(f"ID de la instancia: {instance_id}, Nombre: {instance_name}")
+# Lista las instancias en la VPC especificada
+instances_response = ec2_client.describe_instances(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
 
-    except Exception as e:
-        print(f"Error al obtener instancias: {e}")
+# Imprime información sobre las instancias en la VPC seleccionada
+print("\nInstancias en la VPC con ID", vpc_id, ":")
+for reservation in instances_response['Reservations']:
+    for instance in reservation['Instances']:
+        print('ID de la instancia:', instance['InstanceId'])
+        print('Tipo de instancia:', instance['InstanceType'])
+        print('Estado de la instancia:', instance['State']['Name'])
 
-if __name__ == "__main__":
-    # Llama a la función para obtener detalles de todas las instancias
-    obtener_detalles_todas_instancias()
+# Describe las instancias
+response = ec2_client.describe_instances()
+print('Instancias de EC2:', response['Reservations'])
+
+# Describe las zonas de disponibilidad
+ec2 = boto3.client('ec2')
+response = ec2.describe_availability_zones()
+print('Availability Zones:', response['AvailabilityZones'])
+
+# Obtiene la lista de clústeres de ECS
+ecs_client = boto3.client('ecs')
+clusters = ecs_client.list_clusters()
+print('Lista de clústeres de ECS:', clusters['clusterArns'])
