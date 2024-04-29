7306221fb5ded4190283b0cb859920724c9f0dd7
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2024-04-28 21:03:02 -0500

    elimine permisos de ec2

--- infraestructure/modules/iam/main.tf
+++ infraestructure/modules/iam/main.tf
@@ -26,36 +26,6 @@ EOF
   }
 }
 
-# Custom ReadOnly Policy for Ec2 and VPC
-resource "aws_iam_policy" "vpc_read_only_policy" {
-  name        = "policy-${var.name_role}"
-  description = "Permisos de lectura para Ec2 y VPC"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          # EC2 Read Only Actions
-          "ec2:Describe*",
-          # VPC Read Only Actions
-          "ec2:DescribeVpcs",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeRouteTables",
-          "ec2:DescribeNetworkAcls",
-          "ec2:DescribeInternetGateways",
-          "ec2:DescribeDhcpOptions",
-          "ec2:DescribeVpnConnections",
-          "ec2:DescribeVpnGateways",
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
 # Attachment of the policy to the IAM role
 resource "aws_iam_role_policy_attachment" "TaskExecutionRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
@@ -67,13 +37,4 @@ resource "aws_iam_role_policy_attachment" "TaskExecutionRole" {
 }
 
 
-# Attachment of the EC2 and VPC ReadOnly Policy to the IAM Role
-resource "aws_iam_role_policy_attachment" "vpc_read_only_policy_attachment" {
-  policy_arn = aws_iam_policy.vpc_read_only_policy.arn
-  role = aws_iam_role.ecsTaskExecutionRole.name
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
 
